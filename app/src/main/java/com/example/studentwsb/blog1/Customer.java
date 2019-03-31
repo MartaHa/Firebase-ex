416b45a8ee3aca815ad2dd54524a87c7578af035
MartaHa <helbinmarta@gmail.com>
2019-03-31 11:53:36 +0200

    Firebase added

---
+++ app/src/main/java/com/example/studentwsb/blog1/Customer.java
@@ -0,0 +1,55 @@
+package com.example.studentwsb.blog1;
+
+/**
+ * Created by studentwsb on 2019-03-31.
+ */
+
+public class Customer {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+
+    public Customer(String firstName, String lastName, String email, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Customer() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ app/src/main/java/com/example/studentwsb/blog1/MainActivity.java
@@ -0,0 +1,138 @@
+package com.example.studentwsb.blog1;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "MainActivity";
+
+    private EditText email;
+    private EditText password;
+    private Button login;
+    private Button signOutButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.emailEditText);
+        password = findViewById(R.id.passwordEditText);
+        login = findViewById(R.id.loginButton);
+        signOutButton = findViewById(R.id.signOutButton);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("message");
+
+        databaseReference.setValue("Alamakota");
+
+
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+////                String value = dataSnapshot.getValue(String.class);
+////                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+////                Log.d("log", value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //użytkownik sie zalogował
+                    Log.d(TAG, "użytkownik zalogowany");
+                    Log.d(TAG, "My user: " + user.getEmail());
+                } else {
+                    //uzytkownik niezalogowany
+                    Log.d(TAG, "użytkownik nie jest zalogowany");
+                }
+            }
+        };
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String emailString = email.getText().toString();
+                String passwordString = password.getText().toString();
+
+                if (!emailString.equals("") & !passwordString.equals("")) {
+                    mAuth.signInWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Błędne dane logowania", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(MainActivity.this, "Zalogowano", Toast.LENGTH_LONG).show();
+
+
+                                //zapisywanie do bazy danych
+
+                                Customer customer = new Customer("Jan", "Kowalski", emailString, 45);
+                                databaseReference.setValue(customer);
+
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        signOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Toast.makeText(MainActivity.this, "Logged out", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+}
